66c5a596cc5d146aa03e7be67422dbc7c6a5869a
Florea Eric Flaviu <ericflaviu.florea@gmail.com>
2025-04-25 16:04:16 +0300

    Started working on the movement of the character for the boss scene

---
+++ Scripts/player.gd
@@ -0,0 +1,65 @@
+extends CharacterBody2D
+
+@export var moveSpeed = 300
+@export var slideCooldown = 1.0 # seconds
+@export var slideSpeed = 3.0 # multiplier
+@export var slideDuration = 10 # in frames
+
+var is_moving = false
+var can_slide = true
+var is_sliding = false
+var slideFrames = 0
+var slide_direction = Vector2.ZERO
+var last_move_direction = Vector2.ZERO
+
+func _ready():
+	if $SlideTime is Timer:
+		$SlideTime.wait_time = slideCooldown
+		$SlideTime.one_shot = true
+	else:
+		push_error("SlideTime node is missing or not a Timer!")
+
+func _physics_process(_delta):
+	var input_dir = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input_dir.x += 1
+	if Input.is_action_pressed("move_up"):
+		input_dir.y -= 1
+	if Input.is_action_pressed("move_down"):
+		input_dir.y += 1
+	
+	if Input.is_action_just_pressed("slide") and can_slide and last_move_direction != Vector2.ZERO:
+		can_slide = false
+		is_sliding = true
+		slideFrames = slideDuration
+		slide_direction = last_move_direction
+		print("Slide cooldown starting")
+		$SlideTime.start()
+	
+	input_dir = input_dir.normalized()
+	is_moving = input_dir != Vector2.ZERO
+	
+	if is_moving:
+		last_move_direction = input_dir
+		#print("Last direction updated to:", last_move_direction)
+	
+	if is_sliding:
+		velocity = slide_direction * moveSpeed * slideSpeed
+		slideFrames -= 1
+		if slideFrames <= 0:
+			is_sliding = false
+	else:
+		velocity = input_dir * moveSpeed
+	move_and_slide()
+
+func _on_SlideTime_timeout():
+	print("Slide is ready")
+	can_slide = true
+
+
+func _on_slide_time_timeout() -> void:
+	print("Slide is ready")
+	can_slide = true # Replace with function body.
