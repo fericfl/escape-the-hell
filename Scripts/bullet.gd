9555cdc72721e6ca1eaec0d6a0a7dbe63271327c
Florea Eric Flaviu <ericflaviu.florea@gmail.com>
2025-05-22 15:02:25 +0300

    Added sprite and rotation to bullet and made collision changes

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -9,20 +9,24 @@ func _ready():
 
 func _physics_process(delta):
 	position += direction * speed * delta
-	
-	if not get_viewport_rect().has_point(global_position):
-			print("Bullet out of view, deleting")
-			queue_free()
 
 func set_direction(dir: Vector2):
 	direction = dir.normalized()
+	rotation = direction.angle()
+	$Bullet.flip_v = direction.x < 0
 	visible = true
 
 func _on_body_entered(body):
-	if bullet_owner == "Player" and body.is_in_group("Boss"):
-		body.take_damage()
-		queue_free()
+	if bullet_owner == "Player": 
+		if body.is_in_group("Boss"):
+			body.take_damage()
+			queue_free()
+		elif body.is_in_group("Wall"):
+			queue_free()
 	
-	if bullet_owner == "Boss" and body.is_in_group("Player"):
-		body.take_damage()
-		queue_free()
+	if bullet_owner == "Boss":
+		if body.is_in_group("Player"):
+			body.take_damage()
+			queue_free()
+		elif body.is_in_group("Wall"):
+			queue_free()
