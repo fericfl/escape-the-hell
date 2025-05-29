cf002dbf87176739be388bc71cb0e46ee3c6bd25
Florea Eric Flaviu <ericflaviu.florea@gmail.com>
2025-05-29 16:10:06 +0300

    Finalized the maze structure and style started working on adding light.

--- Scripts/light_maze.gd
+++ Scripts/light_maze.gd
@@ -4,14 +4,17 @@ extends Node2D
 @onready var wall_tilemap = $Walls
 @onready var floor_tilemap = $Floor
 
-const MAZE_SIZE = Vector2i(50,50)
-const FLOOR_TILE = Vector2i(1,0)
+const MAZE_SIZE = Vector2i(80,25)
+const FLOOR_TILES = [Vector2i(1,0), Vector2i(0,0), Vector2i(2,0), Vector2i(1,1)]
 const TERRAIN_SET = 0
 const WALL_TERRAIN_ID = 0
 
 var maze = []
 var visited = []
 var directions = [Vector2i(0, -2), Vector2i(0, 2), Vector2i(-2, 0), Vector2i(2,0)]
+
+var START_POS = Vector2i(1, MAZE_SIZE.y / 2)
+var END_POS = Vector2i(MAZE_SIZE.x - 2, MAZE_SIZE.y / 2)
 
 func _ready():
 	randomize()
@@ -26,9 +29,12 @@ func generate_maze():
 		for x in range(MAZE_SIZE.x):
 				maze[y].append(false)
 
-	var start = Vector2i(1,1)
-	visited.append(start)
-	carve(start)
+	visited.append(START_POS)
+	carve(START_POS)
+	
+	maze[END_POS.y][END_POS.x] = true
+	var bridge = (START_POS + END_POS) / 2
+	maze[bridge.y][bridge.x] = true
 	
 	wall_tilemap.clear()
 	var wall_positions = []
@@ -37,11 +43,13 @@ func generate_maze():
 		for x in range(MAZE_SIZE.x):
 			var pos = Vector2i(x,y)
 			if maze[y][x]:
-				floor_tilemap.set_cell(pos, 0, FLOOR_TILE)
+				floor_tilemap.set_cell(pos, 0, FLOOR_TILES.pick_random())
 			else:
 				wall_positions.append(pos)
-				floor_tilemap.set_cell(pos, 0, FLOOR_TILE)
+				floor_tilemap.set_cell(pos, 0, FLOOR_TILES.pick_random())
 	wall_tilemap.set_cells_terrain_connect(wall_positions, 0, 0)
+	floor_tilemap.set_cell(END_POS, 0, Vector2i(0,5))
+
 func carve(pos: Vector2i):
 	maze[pos.y][pos.x] = true
 	directions.shuffle()
@@ -57,6 +65,5 @@ func is_inside_maze(pos: Vector2i) -> bool:
 	return pos.x > 0 and pos.x < MAZE_SIZE.x - 1 and pos.y > 0 and pos.y < MAZE_SIZE.y - 1
 
 func spawn_player():
-	var start_tile = visited[0]
-	var start_pos = wall_tilemap.map_to_local(start_tile)
+	var start_pos = wall_tilemap.map_to_local(START_POS)
 	player.global_position = wall_tilemap.to_global(start_pos)
